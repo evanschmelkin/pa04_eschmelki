612607c2841386322218fd06c3ff82b38bcb74ca
evanschmelkin <evanschmelkin@gmail.com>
2025-04-15 17:55:46 -0400

    interesitng push

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,14 @@
+class ApplicationController < ActionController::Base
+  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
+  allow_browser versions: :modern
+  def home
+  end
+
+  def about
+  end
+
+  def blog
+  end
+
+
+end
